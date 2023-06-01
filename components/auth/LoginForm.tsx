74af700530740051a4b868d636429dddac300c9d
nilsmanc <etereaiberia@gmail.com>
2023-06-01 08:58:13 +0300

    sign in

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import styles from './Auth.module.scss'
-import { Button, Form, Input, message } from 'antd'
+import { Button, Form, Input, notification } from 'antd'
+import { LoginFormDTO } from '@/api/dto/auth.dto'
+
+import * as Api from '@/api'
+import { setCookie } from 'nookies'
 
 export const LoginForm: React.FC = () => {
-  const onSubmit = (values: any) => {
-    console.log(values)
+  const onSubmit = async (values: LoginFormDTO) => {
+    try {
+      const { token } = await Api.auth.login(values)
+
+      notification.success({
+        message: 'Success',
+        description: 'To admin-panel...',
+        duration: 2,
+      })
+
+      setCookie(null, '_token', token, {
+        path: '/',
+      })
+    } catch (err) {
+      console.warn('LoginForm', err)
+
+      notification.error({
+        message: 'Error',
+        description: 'Incorrect login or password',
+        duration: 2,
+      })
+    }
   }
 
   return (
