42f18f476fc7cd14836e5038880d74eb25059c09
nilsmanc <etereaiberia@gmail.com>
2023-06-08 08:16:24 +0300

    utils

--- components/FileCard/index.tsx
+++ components/FileCard/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import styles from './FileCard.module.scss'
+import { getExtensionFromFileName } from '@/utils/getExtensionFromFileName'
+import { getColorByExtension } from '@/utils/getColorByExtension'
+import { FileTextOutlined } from '@ant-design/icons'
 
 interface FileCardProps {
   filename: string
@@ -10,11 +13,17 @@ export const FileCard: React.FC<FileCardProps> = ({
   originalName,
   filename,
 }) => {
+  const ext = getExtensionFromFileName(filename)
+
+  const color = getColorByExtension(ext)
+  const classColor = styles[color]
+
   return (
     <div className={styles.root}>
       <div className={styles.icon}>
-        <i></i>
-        <img className={styles.image} src={'.'} alt="File" />
+        <i className={classColor}>{ext}</i>
+
+        <FileTextOutlined />
       </div>
       <span>{originalName}</span>
     </div>
